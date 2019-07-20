da529920e2218be11336cb00a4f11eb035b70b9b
tiffanywest@gmail.com <tiffanywest520@gmail.com>
2019-07-20 12:10:53 -0400

    routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module'; // Added here
+
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 
