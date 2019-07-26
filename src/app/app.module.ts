e53c3b426e03a4a63cc63839210505f80234069e
billyclara <you@example.com>
2019-07-25 20:00:02 -0400

    updating angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,62 +1,3 @@
-<<<<<<< HEAD
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { MenuComponent } from './components/menu/menu.component';
-import { SeatsComponent } from './components/seats/seats.component';
-import { MoviesComponent } from './components/movies/movies.component';
-
-import { MoviesService} from './services/movies.service';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-import { TooltipModule } from 'ngx-bootstrap';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
-
-import { WelcomeComponent } from './components/welcome/welcome.component';
-<<<<<<< HEAD
-import { SeatsService } from './services/seats.service';
-=======
-import { MenuService } from './services/menu.service';
-import { CheckoutComponent } from './components/checkout/checkout.component';
->>>>>>> refs/remotes/origin/master
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    MenuComponent,
-    SeatsComponent,
-    MoviesComponent,
-    WelcomeComponent,
-    CheckoutComponent,
-
-    
-    
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    TooltipModule.forRoot(),
-    BrowserAnimationsModule,
-    NgbModule, HttpClientModule,
-    FormsModule
-
-  ],
-<<<<<<< HEAD
-  providers: [MoviesService,
-  SeatsService],
-=======
-  providers: [MoviesService, MenuService],
->>>>>>> refs/remotes/origin/master
-  bootstrap: [AppComponent]
-})
-export class AppModule { }
-=======
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
@@ -109,4 +50,3 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
   bootstrap: [AppComponent]  
 })
 export class AppModule { }
->>>>>>> 8f55c9fd09d8d8045b260852013f74ea286da6f6
